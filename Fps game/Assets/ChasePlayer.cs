098aa54329c48c0bd8c7443851640923ab3e43f8
Main <martinconcetti97@gmail.com>
2024-04-29 19:54:05 -0300

    Basic enemies chase the player

---
+++ Fps game/Assets/ChasePlayer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayer : MonoBehaviour
+{
+    [SerializeField] private NavMeshAgent enemy;
+    [SerializeField] private Transform player;
+
+    /// <summary>
+    /// Gets current nav mesh in the game object
+    /// </summary>
+    private void Start()
+    {
+        enemy = GetComponent<NavMeshAgent>();
+    }
+
+    /// <summary>
+    /// Moves game object to the player transform
+    /// </summary>
+    private void Update()
+    {
+        enemy.SetDestination(player.position);
+        Debug.Log("We mover");
+    }
+
+}
